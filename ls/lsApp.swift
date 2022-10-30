d6d626ced8d72a89fad998647887ce0d2ff95d45
Lamia AlDossari <lamia1albattal@gmail.com>
2022-10-30 13:14:02 +0300

    Initial Commit

---
+++ ls/lsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lsApp.swift
+//  ls
+//
+//  Created by Lamia Aldossari on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct lsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
